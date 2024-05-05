baab928f671ee8207edafe9e855a7f53c9c65ccd
AhmedYTawfik <ahmedyasser55577@gmail.com>
2024-05-05 22:27:34 +0300

    bnd7k

---
+++ youth-like-fruits/src/AdminPage/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ElevateAppBar from '../Components/ElevateAppBar/ElevateAppBar'
+import PersistentDrawerLeft from './PersistentDrawerLeft'
+
+function AdminPage() {
+  return (
+    <div>
+        {/* <ElevateAppBar/> */}
+        <PersistentDrawerLeft/>
+    </div>
+  )
+}
+
+export default AdminPage
