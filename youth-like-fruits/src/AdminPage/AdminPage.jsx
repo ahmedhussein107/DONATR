1f8f620f99ffe508884f8194efb8407ffcade6ea
AhmedKamal18 <ahmedelbakly95@gmail.com>
2024-05-06 17:34:09 +0300

    add background for statistics

--- youth-like-fruits/src/AdminPage/AdminPage.jsx
+++ youth-like-fruits/src/AdminPage/AdminPage.jsx
@@ -11,12 +11,23 @@ function AdminPage() {
     <div className='container'>
         <ElevateAppBar/>
         <div style={{display : 'flex' , msFlexDirection : 'row'}}>
-        <div style = {{display: 'flex' , flexDirection: 'row'}}>
-          <SideBar/>
-        </div>
-       <div  style={{display : 'flex' , flexDirection : 'row',marginTop :'15%'}}>
-       <PieActiveArc/>
-       <StackBars/>
+        <SideBar/>
+       <div style = {{display : 'flex' , flexDirection : 'column',marginTop :'2%'}}>
+          <p style={{
+          fontSize: '44px',
+          fontFamily: 'Josefin Sans',
+          fontWeight: '400',
+          color: '#292F33',
+          marginLeft: '5%',
+        }}>Overview</p>
+          <div  style={{display : 'flex' , flexDirection : 'row',marginTop :'2%', position: 'relative'}}>
+          <div style={{minWidth: '35%', backgroundColor: '#ffffff',  marginLeft: '1%', alignContent: 'center', borderRadius: '5px'}}>
+          <PieActiveArc/>
+          </div>  
+          <div style={{padding: '1%', backgroundColor: '#ffffff',  marginLeft: '1%',borderRadius: '5px'}}>
+          <StackBars/>
+          </div>
+          </div>
        </div>
        </div>
         <Footer/>
