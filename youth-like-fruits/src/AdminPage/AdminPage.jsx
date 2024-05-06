c79659af7aa81eb0e637a24b0387925c941d567b
AhmedYTawfik <ahmedyasser55577@gmail.com>
2024-05-06 17:03:16 +0300

    bnd7k

--- youth-like-fruits/src/AdminPage/AdminPage.jsx
+++ youth-like-fruits/src/AdminPage/AdminPage.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import ElevateAppBar from '../Components/ElevateAppBar/ElevateAppBar'
 import SideBar from './SideBar'
 import Footer from '../Components/Footer/Footer'
+import PieActiveArc from './Dashboard/PieActiveArc'
+import StackBars from './Dashboard/StackBars'
 
 function AdminPage() {
   
   return (
     <div className='container'>
         <ElevateAppBar/>
+        <div style={{display : 'flex' , msFlexDirection : 'row'}}>
         <div style = {{display: 'flex' , flexDirection: 'row'}}>
           <SideBar/>
         </div>
+       <div  style={{display : 'flex' , flexDirection : 'row',marginTop :'15%'}}>
+       <PieActiveArc/>
+       <StackBars/>
+       </div>
+       </div>
         <Footer/>
+
     </div>
   )
 }
