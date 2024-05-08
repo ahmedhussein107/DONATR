510134c149c13475baf9dc9a5cfa7fb0dee04b56
AhmedKamal18 <ahmedelbakly95@gmail.com>
2024-05-08 22:05:59 +0300

    add donor register form

--- youth-like-fruits/src/App.jsx
+++ youth-like-fruits/src/App.jsx
@@ -4,11 +4,9 @@ import LoginPage from "./Loginpage/LoginPage"
 import RegisterPage from "./RegisterPage/Register"
 import AdminPage from "./AdminPage/AdminPage"
 
-import ElevateAppBar from "./Components/ElevateAppBar/ElevateAppBar.jsx"
-import Footer from "./Components/Footer/Footer"
-import LeftSide from "./RegisterPage/Layouts/LeftSide.jsx"
 import OrganizationReg from "./RegisterPage/OrganizationReg.jsx"
 import DonorsList from "./AdminPage/DonorsList/DonorsList.jsx"
+import DonorReg from "./RegisterPage/DonorReg.jsx"
 
 
 function App() {
@@ -19,7 +17,8 @@ function App() {
         <Route path='/login' element ={<LoginPage/>}/>
         <Route path='/register' element = {<RegisterPage/>}/>
         <Route path='/admin' element = {<AdminPage/>}/>
-        <Route path='/register/Organization' element={<OrganizationReg/>}/>
+        <Route path='/register/organization' element={<OrganizationReg/>}/>
+        <Route path='/register/donor' element={<DonorReg/>}/>
         <Route path='admin/donors-list' element={<DonorsList/>}/>
       </Routes>
     </>
