70f66c6f48fa6dd2dd804602480e8d9101ef0383
Yousseffouda6 <yamr1517@gmail.com>
2024-05-11 10:03:06 +0300

    event page

---
+++ youth-like-fruits/src/EventPage/EventPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ElevateAppBar from '../Components/ElevateAppBar/ElevateAppBar'
+import App from './EventLayout'
+
+function EventPage() {
+
+
+    return (
+    <div className='container'>
+      <ElevateAppBar/>
+      <App/>
+    </div>
+    )
+  }
+
+export default EventPage
