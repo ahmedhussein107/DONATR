b8d76a42a7ae3db9bb031392820e7b57646c181d
AhmedKamal18 <ahmedelbakly95@gmail.com>
2024-05-11 20:19:28 +0300

    test data generator

--- youth-like-fruits/src/material-kit/_mock/donor.js
+++ youth-like-fruits/src/material-kit/_mock/donor.js
@@ -1,11 +1,11 @@
 import { sample } from 'lodash';
-import { faker } from '@faker-js/faker';
+import { faker, fakerAR } from '@faker-js/faker';
 
 // ----------------------------------------------------------------------
 
 export const donors = [...Array(16)].map((_, index) => ({
     id: faker.string.uuid(),
-    name: faker.person.fullName(),
+    name: fakerAR.person.fullName(),
     email: faker.internet.email(),
     day: faker.number.int({min: 1 , max: 30}),
     month: faker.number.int({min: 1 , max: 12}),
