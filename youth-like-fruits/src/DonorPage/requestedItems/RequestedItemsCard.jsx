5787c2c282d1120ea535ab2f6d667e164531ed4c
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2024-05-12 15:07:56 +0300

    add requested-items-filter dynamic

--- youth-like-fruits/src/DonorPage/requestedItems/RequestedItemsCard.jsx
+++ youth-like-fruits/src/DonorPage/requestedItems/RequestedItemsCard.jsx
@@ -5,6 +5,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
+import clothesIcon from '../../assets/Medical.png';
+import toysIcon from '../../assets/Medical.png';
+import foodIcon from '../../assets/Medical.png';
+import medicalIcon from '../../assets/Medical.png';
+import schoolIcon from '../../assets/Medical.png';
+import bloodIcon from '../../assets/Medical.png';
+
 export default function RequestedItemsCard(props) {
   const day = props.day;
   const month = props.month;
@@ -13,6 +20,25 @@ export default function RequestedItemsCard(props) {
   const onClick = props.onClick;
   const type = props.type;
   const details = props.details;
+
+  const getImage = () => {
+    switch (type) {
+        case 'Clothes':
+            return clothesIcon;
+        case 'Toys':
+            return toysIcon;
+        case 'Food':
+            return foodIcon;
+        case 'Medical Supplies':
+            return medicalIcon;
+        case 'School Supplies':
+            return schoolIcon;
+        case 'Blood Donations':
+            return bloodIcon;
+        default:
+            return clothesIcon;
+    }
+  }
 
   return (
     <Card sx={{maxHeight: 600, maxWidth: 600 }} onClick={onClick}>
@@ -26,6 +52,7 @@ export default function RequestedItemsCard(props) {
           component="img"
           height="auto" 
           width="100%" 
+          image={getImage()}
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
