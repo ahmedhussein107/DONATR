cb05d5ac9d31f2ac8fcdff38ea3b65326c4fd46d
AhmedYTawfik <ahmedyasser55577@gmail.com>
2024-05-12 18:51:47 +0300

    fix conflict

--- youth-like-fruits/src/OrganizationPage/OrganizationPage.jsx
+++ youth-like-fruits/src/OrganizationPage/OrganizationPage.jsx
@@ -9,20 +9,9 @@ import NotificationsPopover from '../AdminPage/Dashboard/Notifications'
 import AccountPopover from '../AdminPage/Dashboard/profile'
 import Stack from '@mui/material/Stack';
 import HomeIcon from '@mui/icons-material/Home';
-import ListIcon from '@mui/icons-material/List';
-import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
-import ChecklistRoundedIcon from '@mui/icons-material/ChecklistRounded';
-import SelectVariants from '../AdminPage/Dashboard/SelectVariants'
-import OutlinedCard from '../AdminPage/Dashboard/OutlinedCard'
-import StackBars from '../AdminPage/Dashboard/StackBars'
+import Profile from './Profile.jsx'
 
 function OrganizationPage() {
-
-
-
-
-
-    // My states
     const [selectedOption, setSelectedOption] = useState(' ');
     const [showAlert, setShowAlert] = useState(false);
 
@@ -30,40 +19,12 @@ function OrganizationPage() {
 
         <Stack direction="row" alignItems="center" spacing={1}>
             <NotificationsPopover />
-            <AccountPopover />
+            <AccountPopover dir={'/OrganizationPage'} dir2={'/Settings7'} />
         </Stack>
     ]
 
-    const handleFormSubmit = () => {
-        setShowAlert(true);
-        setSelectedOption(' ');
-    }
-
-    const handleOptionSelection = (x) => {
-        setSelectedOption(x);
-        setShowAlert(false);
-    }
 
     const fontSize = '3vh';
-    const menuItem = [
-        {
-
-            path: "/organization",
-            name: "Dashboard",
-            icon: <HomeIcon style={{ fontSize: fontSize, fill: '#D5E0E6' }} />
-        },
-        {
-            path: "/organization/create-post",
-            name: "Create donation posts",
-            icon: <ListIcon style={{ fontSize: fontSize, fill: '#D5E0E6' }} />
-        },
-        {
-            path: "/organization/view-posts",
-            name: "View donation posts",
-            icon: <ListIcon style={{ fontSize: fontSize, fill: '#D5E0E6' }} />
-        },
-
-    ]
 
     return (
 
@@ -76,7 +37,7 @@ function OrganizationPage() {
             {/* Body */}
             <div className='body' style={{}}>
 
-                <SideBar menuItem={menuItem} />
+                <SideBar />
                 <div style={{ display: 'flex', flexDirection: 'column', padding: '2% 4%', width: '100%' }}>
                     <p className='title' style={{
                         fontSize: '44px',
@@ -85,30 +46,15 @@ function OrganizationPage() {
                         color: '#292F33',
                     }}>Create Donation Post</p>
 
+                    <Profile />
 
 
 
-                    <div style={{
-                        width: '100%',
-                        backgroundColor: '#D5E0E6',
-                        borderRadius: '5px',
-                        boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.3)',
-                        padding: '20px'
-                    }}>
-                        <p style={{
-                            fontSize: '24px',
-                            fontFamily: 'Josefin Sans',
-                            fontWeight: 'bold',
-                            color: '#000000',
-                            marginBottom: '10px'
-                        }}>Select</p>
-
-                    </div>
 
                 </div>
 
 
-                <Profile />
+
             </div>
             <Footer />
         </div>
@@ -117,10 +63,3 @@ function OrganizationPage() {
 
 export default OrganizationPage
 
-
-const pageTitleStyle = {
-    fontSize: '44px',
-    fontFamily: 'Josefin Sans',
-    fontWeight: '400',
-    color: '#292F33',
-}
